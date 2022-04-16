3fe764aef72955449c45f9775535741b5b622253
Mario Yordanov <mario@Marios-MacBook-Pro.local>
2022-04-17 00:25:11 +0300

    fin

--- salar.cpp
+++ salar.cpp
@@ -52,6 +52,16 @@ struct Array
             }
         }
 
+        void print()
+        {
+            for(int i = 0; i < length ; i++)
+            {
+                cout<<this->list[i]<< " ";
+            }
+
+            cout<<endl;
+        }
+
     private:
         void resize()
         {
@@ -70,24 +80,23 @@ struct Array
         }
 };
 
-
-void readArraysFromFile(Array* &p1, Array* &p2)
+void readArraysFromFile(Array& p1, Array& p2)
 {
     std::ifstream input(fileName);
     std::string line;
-    Array** arr[] = {&p1, &p2};
+    Array* arr[] = {&p1, &p2};
 
     for(int i = 0; i < 2; i++)
     {
-        Array** pArr = arr[i];
-        *pArr = new Array;
+        //Array** pArr = arr[i];
+        //*pArr = new Array;
         getline(input, line);
         istringstream iss(line);
 
         int element;
         while(iss >> element)
         {
-            (*pArr)->add(element);
+            arr[i]->add(element);
         }
     }
 
@@ -110,11 +119,77 @@ void writeDataToFile(const Array& arr1, const Array& arr2)
     }
 }
 
+void shellSort(Array& array)
+{
+    for (int gap = array.length/2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < array.length; i += 1)
+        {
+            int temp = array.list[i];
+ 
+            int j;           
+            for (j = i; j >= gap && array.list[j - gap] > temp; j -= gap)
+                array.list[j] = array.list[j - gap];
+            
+            array.list[j] = temp;
+        }
+    }
+}
+
+void showMenu()
+{
+    cout << "Izberete:"<<endl;
+    cout << "1. Procheti danni ot fail."<<endl;
+    cout << "2. Zapis na danni vuv fail." << endl;
+    cout << "3. Sortirane na po metoda na Shell." << endl;
+    cout << "4. Printirane na spisucite." << endl;
+    cout << "Izberete q za izhod." << endl;
+
+}
+
 int main()
 {
     Array p1;
-    p1.add(100);
-    p1.add(200);
+    Array p2;
 
-    writeDataToFile(p1, p1);
+    showMenu();
+    char input;
+    cin>>input;
+
+    while(input != 'q' || input != 'Q')
+    {
+        switch( input )
+        {
+            case '1' : 
+            {
+                readArraysFromFile(p1, p2);
+                break;
+            }
+            case '2' : 
+            {
+                writeDataToFile(p1, p2);
+                break;
+            }
+            case '3' :
+            {
+                shellSort(p1);
+                shellSort(p2);
+                break;
+            }
+            case '4':
+            {
+                p1.print();
+                p2.print();
+                break;
+            }
+            default: 
+            {
+                cout<<"Vuvedete validna opciq ot 1 do 4"<<endl;
+                break;
+            }
+        }
+
+        cin >> input;
+        showMenu();
+    }
 }
